4deedf9903563b3084cf97968cd1f6eee94f5cd3
jdpace <jared@codewordstudios.com>
2009-09-29 11:04:50 -0400

    Don't freak out if config.yml doesn't exist

--- config/initializers/001_load_config.rb
+++ config/initializers/001_load_config.rb
@@ -2,33 +2,40 @@ require 'erb'
 require 'yaml'
 require 'ostruct'
 
-erb = File.read(File.join(Rails.root, 'config', 'config.yml'))
-yml = ERB.new(erb).result
+begin
+  
+  erb = File.read(File.join(Rails.root, 'config', 'config.yml'))
+  yml = ERB.new(erb).result
 
-# Load application config
-config = YAML.load(yml)
+  # Load application config
+  config = YAML.load(yml)
 
-# Remove Environment namespaces
-config.delete('development')
-config.delete('test')
-config.delete('staging')
-config.delete('production')
+  # Remove Environment namespaces
+  config.delete('development')
+  config.delete('test')
+  config.delete('staging')
+  config.delete('production')
 
-# Merge back in current environment
-config.merge!(YAML.load(yml)[Rails.env]) if YAML.load(yml)[Rails.env].is_a?(Hash)
+  # Merge back in current environment
+  config.merge!(YAML.load(yml)[Rails.env]) if YAML.load(yml)[Rails.env].is_a?(Hash)
 
-# Define true? style methods
-truthy = {}
-config.each do |key,value|
-  truthy[key.to_s + "?"] = value if value.is_a?(TrueClass) || value.is_a?(FalseClass)
+  # Define true? style methods
+  truthy = {}
+  config.each do |key,value|
+    truthy[key.to_s + "?"] = value if value.is_a?(TrueClass) || value.is_a?(FalseClass)
+  end
+  config.merge!(truthy)
+
+  # Convert all keys and sub keys to symbols
+  config.symbolize_keys!
+  config.each do |key, value|
+    config[key].symbolize_keys! if value.kind_of?(Hash)
+  end
+
+  # Create NewsStand OpenStruct
+  ::App = OpenStruct.new(config)
+
+rescue Errno::ENOENT
+  # config.yml file does not exist
+  puts "Config file not found. Please copy and modify config/config.example.yml to config/config.yml"
 end
-config.merge!(truthy)
-
-# Convert all keys and sub keys to symbols
-config.symbolize_keys!
-config.each do |key, value|
-  config[key].symbolize_keys! if value.kind_of?(Hash)
-end
-
-# Create NewsStand OpenStruct
-::App = OpenStruct.new(config)
