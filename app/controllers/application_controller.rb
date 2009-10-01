32df713a871d0e7a1abb4feaf976d23d1c231c0c
jdpace <jared@codewordstudios.com>
2009-10-01 17:47:30 -0400

    Authenticate site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :authenticate
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  protected
+  
+     def authenticate
+       authenticate_or_request_with_http_basic('Enventys Wordpress Admin') do |username, password|
+         username == App.authentication[:username] && password == App.authentication[:password]
+       end
+     end
 end
